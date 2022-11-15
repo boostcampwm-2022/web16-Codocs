1bd32ec0020e379ca601a6b4353980ccd7c10888
k-seok <rltjr1092@gmail.com>
2022-11-15 18:12:55 +0900

    chore: #8 CI 실패 테스트 정상화
    Co-authored-by: rang21c <rang21c@naver.com>

--- backend/api/src/app.controller.spec.ts
+++ backend/api/src/app.controller.spec.ts
@@ -16,7 +16,7 @@ describe('AppController', () => {
 
   describe('root', () => {
     it('adds 1 + 2 to equal 3', () => {
-      expect(() => 1 + 2).toBe(2);
+      expect(() => 1 + 2).toBe(3);
     });
   });
 });
