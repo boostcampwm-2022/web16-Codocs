b24f6fe6512824ef214fc2968c7bda8193e63a6b
iyu88 <iyu88@naver.com>
2022-11-15 14:49:29 +0900

    feat: #2 react-router 설정
    프론트엔드 react-router 경로를 설정했습니다.
    
    Co-authored-by: komplamoose2 <iamjewel4445@gmail.com>

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,7 +1,24 @@
 import React from 'react';
+import { Routes, Route } from 'react-router-dom';
 
 function App() {
-  return <div className="App">React</div>;
+  return (
+    <div className="App">
+      <Routes>
+        <Route path="/" element="" />
+        <Route path="/document" element="">
+          <Route element="">
+            <Route path="main" element="" />
+            <Route path="private" element="" />
+            <Route path="shared" element="" />
+            <Route path="bookmark" element="" />
+            <Route path="trash" element="" />
+          </Route>
+          <Route path="/document/:document_id" element="" />
+        </Route>
+      </Routes>
+    </div>
+  );
 }
 
 export default App;
