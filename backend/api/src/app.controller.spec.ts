351910ab88878ceb9475de8d1d14e9152ad63ec7
k-seok <rltjr1092@gmail.com>
2022-11-15 17:39:18 +0900

    chore: #8 CI 실패 테스트 다시 수정
    Co-authored-by: rang21c <rang21c@naver.com>

--- backend/api/src/app.controller.spec.ts
+++ backend/api/src/app.controller.spec.ts
@@ -15,8 +15,8 @@ describe('AppController', () => {
   });
 
   describe('root', () => {
-    it('should return "Hello World!"', () => {
-      expect(appController.getHello()).toBe('Hello World!');
+    it('adds 1 + 2 to equal 3', () => {
+      expect(() => 1 + 2).toBe(2);
     });
   });
 });
