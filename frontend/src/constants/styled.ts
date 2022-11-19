1d0a72449ba00a0a6cada44016858494b52b186c
komplamoose2 <iamjewel4445@gmail.com>
2022-11-19 16:32:26 +0900

    feat: #3 styled-components Color 상수 파일 설정
    프로그램에서 사용되는 색상들을 상수화하였습니다.

---
+++ frontend/src/constants/styled.ts
@@ -0,0 +1,13 @@
+export const COLOR_BACKGROUND = '#FEFEFF';
+
+export const COLOR_ACTIVE = '#3A7DFF';
+
+export const COLOR_BLACK = '#222222';
+
+export const COLOR_BORDER = '#BBBBBB';
+
+export const COLOR_WHITE = '#FFFFFF';
+
+export const COLOR_UNSELECTED = '#A5A5A5';
+
+export const COLOR_CAUTION = '#FF5757';
