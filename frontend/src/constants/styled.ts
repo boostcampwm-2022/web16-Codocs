3e085a9f0626629952b8df68df3650947c65cff0
komplamoose2 <iamjewel4445@gmail.com>
2022-11-19 16:47:45 +0900

    feat: #3 아이콘 에셋 추가 및 css filter 상수화

--- frontend/src/constants/styled.ts
+++ frontend/src/constants/styled.ts
@@ -11,3 +11,12 @@ export const COLOR_WHITE = '#FFFFFF';
 export const COLOR_UNSELECTED = '#A5A5A5';
 
 export const COLOR_CAUTION = '#FF5757';
+
+export const FILTER_TO_ACTIVE =
+  'invert(46%) sepia(76%) saturate(4291%) hue-rotate(209deg) brightness(105%) contrast(101%)';
+
+export const FILTER_TO_CAUTION =
+  'filter: invert(30%) sepia(81%) saturate(1539%) hue-rotate(337deg) brightness(105%) contrast(90%);';
+
+export const FILTER_TO_BLACK =
+  'filter: invert(0%) sepia(17%) saturate(37%) hue-rotate(317deg) brightness(103%) contrast(73%)';
