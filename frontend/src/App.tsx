fe17c39a310bc2baa36cc12ffeb95726529cf904
komplamoose2 <iamjewel4445@gmail.com>
2022-11-19 17:52:30 +0900

    fix: dummy test 코드 추가
    ci에서 test 코드가 없어서 test fail하는 문제 해결하기 위해 dummy 테스트 추가

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,28 +1,31 @@
 import React from 'react';
-import { Routes, Route } from 'react-router-dom';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import { RecoilRoot } from 'recoil';
 import GlobalStyles from './GlobalStyles';
 
 function App() {
   return (
-    <RecoilRoot>
-      <div className="App">
-        <GlobalStyles />
-        <Routes>
-          <Route path="/" element="" />
-          <Route path="/document" element="">
-            <Route element="">
-              <Route path="main" element="" />
-              <Route path="private" element="" />
-              <Route path="shared" element="" />
-              <Route path="bookmark" element="" />
-              <Route path="trash" element="" />
+    <BrowserRouter>
+      <RecoilRoot>
+        <div className="App">
+          <GlobalStyles />
+          test
+          <Routes>
+            <Route path="/" element="" />
+            <Route path="/document" element="">
+              <Route element="">
+                <Route path="main" element="" />
+                <Route path="private" element="" />
+                <Route path="shared" element="" />
+                <Route path="bookmark" element="" />
+                <Route path="trash" element="" />
+              </Route>
+              <Route path="/document/:document_id" element="" />
             </Route>
-            <Route path="/document/:document_id" element="" />
-          </Route>
-        </Routes>
-      </div>
-    </RecoilRoot>
+          </Routes>
+        </div>
+      </RecoilRoot>
+    </BrowserRouter>
   );
 }
 
