6a417cf11a97962497c36c65a37e8d3d2358a36a
komplamoose2 <iamjewel4445@gmail.com>
2022-11-19 22:25:01 +0900

    style: 클라이언트 라우팅 코드 Router.tsx로 분리, 폴더구조 설정
    App.tsx에서 담당했던 클라이언트 라우팅을 역할 구분을 위해 Router.tsx로 분리했습니다.
    초기에 설정한 폴더 구조를 지키기 위해 .gitkeep 파일을 생성해 빈 파일이여도 삭제되지 않도록 했습니다.

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,32 +1,13 @@
 import React from 'react';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import { RecoilRoot } from 'recoil';
-import GlobalStyles from './GlobalStyles';
+import Router from './Router';
 
-function App() {
+const App = () => {
   return (
-    <BrowserRouter>
-      <RecoilRoot>
-        <div className="App">
-          <GlobalStyles />
-          test
-          <Routes>
-            <Route path="/" element="" />
-            <Route path="/document" element="">
-              <Route element="">
-                <Route path="main" element="" />
-                <Route path="private" element="" />
-                <Route path="shared" element="" />
-                <Route path="bookmark" element="" />
-                <Route path="trash" element="" />
-              </Route>
-              <Route path="/document/:document_id" element="" />
-            </Route>
-          </Routes>
-        </div>
-      </RecoilRoot>
-    </BrowserRouter>
+    <RecoilRoot>
+      <Router />
+    </RecoilRoot>
   );
-}
+};
 
 export default App;
