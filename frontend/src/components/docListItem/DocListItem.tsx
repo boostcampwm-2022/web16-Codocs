a1c26e200691180d78aaafc69541e2d698f970b9
iyu88 <iyu88@naver.com>
2022-11-21 16:35:46 +0900

    feat: #20 DocListItem 레이아웃 구현
    DocListItem 레이아웃을 styled-components 를 사용하여 구현했습니다.

---
+++ frontend/src/components/docListItem/DocListItem.tsx
@@ -0,0 +1,60 @@
+import React from 'react';
+import styled from 'styled-components';
+import trash from '../../assets/trash.svg';
+import bookmark from '../../assets/bookmark.svg';
+
+const DocListItem = () => {
+  return (<DocListItemWrapper>
+    <Title>알고리즘 스터디 기록</Title>
+    <LowerLayout>
+      <LastVisited>최근 방문일: 2022-11-10</LastVisited>
+      <div>
+        <button>
+          <ImgOnBtn src={trash} alt="삭제하기 버튼"></ImgOnBtn>
+        </button>
+        <button>
+          <ImgOnBtn src={bookmark} alt="즐겨찾기 버튼"></ImgOnBtn>
+        </button>
+      </div>
+    </LowerLayout>
+  </DocListItemWrapper>);
+};
+
+const DocListItemWrapper = styled.div`
+  width: 15em;
+  height: 4.5em;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+  padding: 0.75em;
+  border: 1px solid #B6B6B6;
+  border-radius: 10px;
+  box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25);
+`;
+
+const Title = styled.div`
+  font-weight: 600;
+  font-size: 12px;
+  color: #222222;
+`;
+
+const LowerLayout = styled.div`
+  display: flex;
+  justify-content: space-between;
+  align-items: flex-start;
+  padding-top: 1.5em;
+`;
+
+const LastVisited = styled.div`
+  font-weight: 300;
+  font-size: 10px;
+  color: #A5A5A5;
+`;
+
+const ImgOnBtn = styled.img`
+  width: 0.75em;
+  height: 0.75em;
+  margin-left: 0.5em;
+`;
+
+export { DocListItem };
---
+++ frontend/src/pages/MainPage.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import { DocListItem } from '../components/docListItem';
+
+const MainPage = () => {
+  
+  return (
+    <div>
+      <DocListItem></DocListItem>;
+    </div>
+  );
+};
+
+export default MainPage;
