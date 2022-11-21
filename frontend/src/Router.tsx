8f4df535c29e1c8744d65d0174e1066007918b7e
komplamoose2 <iamjewel4445@gmail.com>
2022-11-21 22:15:12 +0900

    feat: Router.tsx 페이지 라우팅

--- frontend/src/Router.tsx
+++ frontend/src/Router.tsx
@@ -1,22 +1,22 @@
 import React from 'react';
 import GlobalStyles from './GlobalStyles';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import DocumentPage from './pages/DocumentPage';
+import LandingPage from './pages/LandingPage';
 
 const Router = () => {
   return (
     <BrowserRouter>
       <GlobalStyles />
       <Routes>
-        <Route path="/" element="" />
-        <Route path="/document" element="">
-          <Route element="">
-            <Route path="main" element="" />
-            <Route path="private" element="" />
-            <Route path="shared" element="" />
-            <Route path="bookmark" element="" />
-            <Route path="trash" element="" />
-          </Route>
-          <Route path="/document/:document_id" element="" />
+        <Route path="/" element={<LandingPage />} />
+        <Route path="/document">
+          <Route path="main" element="" />
+          <Route path="private" element="" />
+          <Route path="shared" element="" />
+          <Route path="bookmark" element="" />
+          <Route path="trash" element="" />
+          <Route path=":document_id" element={<DocumentPage />} />
         </Route>
       </Routes>
     </BrowserRouter>
---
+++ frontend/src/pages/LandingPage.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const LandingPage = () => {
+  return (
+    <>
+      <div>this is landing page.</div>
+    </>
+  );
+};
+
+export default LandingPage;
