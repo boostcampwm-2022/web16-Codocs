b56610b84be20b3e1b87840ac74e17351572b284
iyu88 <iyu88@naver.com>
2022-11-24 17:09:45 +0900

    feat: #29 코드 에디터에서 Socket 이벤트 발생
    Co-authored-by: Boseok Kang <komplamoose@users.noreply.github.com>
    Co-authored-by: Kiseok <rltjr1092@gmail.com>

--- frontend/src/components/Editor.tsx
+++ frontend/src/components/Editor.tsx
@@ -3,35 +3,66 @@ import SimpleMDEReact from 'react-simplemde-editor';
 import SimpleMDE from 'easymde';
 import CodeMirror from 'codemirror';
 import 'easymde/dist/easymde.min.css';
+import {crdt} from '../core/crdt-linear/crdt';
+import socket from '../core/sockets/sockets';
 
 const Editor = () => {
   const [editor, setEditor] = useState<CodeMirror.Editor | null>(null);
 
-  const getCmInstanceCallback = useCallback((cm: CodeMirror.Editor) => {
-    setEditor(cm);
-  }, []);
-
   useEffect(() => {
-    editor?.on('beforeChange', (instance: CodeMirror.Editor, change: CodeMirror.EditorChange) => {
-      const from = editor.indexFromPos(change.from);
-      const to = editor.indexFromPos(change.to);
-      const content = change.text.join('\n');
+    if (editor) {
+      socket.on('remote-insert', (data) => {
+        console.log('INSERT');
+        crdt.remoteInsert(data, editor.getDoc());
+      });
 
-      console.log(from, to, content);
+      socket.on('remote-delete', (data) => {
+        console.log('DELETE');
+        crdt.remoteDelete(data, editor.getDoc());
+      });
+    }
+
+    editor?.on('beforeChange', (_, change: CodeMirror.EditorChange) => {
+      if (change.origin === 'setValue') {
+        return;
+      }
+      const fromIdx = editor.indexFromPos(change.from);
+      const toIdx = editor.indexFromPos(change.to); // 여러 글자 insert시 다름
+      const content = change.text.join('\n');
+      switch (change.origin) {
+      case '+input':
+      case '*compose': // 한글은 합성 처리 => 원본 글자를 지우고 삽입해야 함.
+        crdt.localInsert(fromIdx, content);
+        break;
+      case '+delete':
+        crdt.localDelete(fromIdx, toIdx);
+        console.log('delete');
+        break;
+      default:
+      }
+      console.log(fromIdx, toIdx, content);
+    });
+    return (()=>{
+      socket.off('remote-insert');
+      socket.off('remote-delete');
     });
   }, [editor]);
 
-  const autofocusNoSpellcheckerOptions = useMemo(() => {
+  const editorOptions = useMemo(() => {
     return {
       autofocus: true,
       spellChecker: false
     } as SimpleMDE.Options;
   }, []);
 
+  const getCmInstanceCallback = useCallback((cm: CodeMirror.Editor) => {
+    setEditor(cm);
+  }, []);
+
   return (
     <>
       <SimpleMDEReact
-        options={autofocusNoSpellcheckerOptions}
+        options={editorOptions}
         getCodemirrorInstance={getCmInstanceCallback}
       />
     </>
