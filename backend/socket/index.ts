ec159b745de291c17829c4f436661b92c9322eaf
iyu88 <iyu88@naver.com>
2022-11-29 18:29:29 +0900

    feat: #34 소켓 방 생성 및 문서 저장 / 불러오기 구현
    클라이언트에서 소켓이 연결될 때 서버에 있는 오브젝트에 CRDT 인스턴스를 생성합니다.
    방 번호를 key 로 하고 CRDT 인스턴스를 value 로 합니다.
    방이 없을 경우 새로운 인스턴스롤 생성하고, 있을 경우 메모리에 존재하는 CRDT struct 데이터를 전달합니다.
    
    Co-authored-by: kiseoky <rltjr1092@gmail.com>
    Co-authored-by: komplamoose2 <iamjewel4445@gmail.com>

--- backend/socket/index.ts
+++ backend/socket/index.ts
@@ -1,19 +1,33 @@
 import * as express from 'express';
 import { Server } from 'socket.io';
 import { createServer } from 'http';
+import { CRDT } from './crdt-linear-server/crdt';
 
 const app = express();
 const httpServer = createServer(app);
 const io = new Server(httpServer, { cors: { origin: '*' } });
 
+const crdts = {};
+// 클라이언트 목록, 룸 목록 관리
+
+
 io.on('connection', client => {
+  client.on('joinroom', (room)=>{
+    if (!crdts.hasOwnProperty(room)) {
+      crdts[room] = new CRDT();
+    }
+    client.emit('new-user', crdts[room].getStruct());
+    client.join(room);
+  })
   client.on('local-insert', data => { 
-    console.log(data);
-    client.broadcast.emit('remote-insert', data);  
+    const roomName = Array.from(client.rooms)[1];
+    crdts[roomName].saveInsert(data); 
+    client.to(roomName).emit('remote-insert', data); 
   });
   client.on('local-delete', data => { 
-    console.log(data);
-    client.broadcast.emit('remote-delete', data);
+    const roomName = Array.from(client.rooms)[1];
+    crdts[roomName].saveDeleteRange(data);
+    client.to(roomName).emit('remote-delete', data);
   });
   client.on('disconnect', () => {
     console.log("Socket disconnected");
