2b35ca844c36ebf361baca9fafcc93cb590c414c
iyu88 <iyu88@naver.com>
2022-11-29 13:05:11 +0900

    test: #31 docList 레이아웃 테스트

---
+++ frontend/src/components/docList/DocList.tsx
@@ -0,0 +1,45 @@
+import React, { useEffect, useState } from 'react';
+import styled from 'styled-components';
+import { DocListItem } from '../docListItem';
+
+interface docListItemProps {
+  id: string,
+  title: string,
+  lastVisited: string
+  role: string
+}
+
+const DocList = () => {
+  const [docList, setDocList] = useState<docListItemProps[]>([]);
+
+  useEffect(() => {
+    const fetchDocList = async () => {
+      // try {
+      // const response = await fetch('/document/main');
+      // const data = await response.json();
+      return new Promise((resolve) => {
+        resolve(setDocList(() => [{id:'1234',
+          title: '알고리즘 스터디 기록',
+          lastVisited: '2022-11-10',
+          role: 'onwer',  
+        }, {id:'5678',
+          title: 'Untitled',
+          lastVisited: '2022-11-19',
+          role: 'edit'}]));
+      });
+      // setDocList(data);
+      // } catch (err) {
+      //   console.log(err);
+      // }
+    };
+    fetchDocList();
+  }, []);
+
+  return (<div>
+    {docList.length && docList.map(doc => {
+      return <DocListItem key={doc.id} id={doc.id} title={doc.title} lastVisited={doc.lastVisited} role={doc.role}></DocListItem>;
+    })}
+  </div>);
+};
+
+export { DocList };
--- frontend/src/pages/MainPage.tsx
+++ frontend/src/pages/MainPage.tsx
@@ -1,38 +1,40 @@
 import React, { useState, useEffect } from 'react';
+import { DocList } from '../components/docList';
 import { DocListItem } from '../components/docListItem';
 
-interface docListItemState {
-  id: string
-  title: string
-  lastVisited: string
-  role: string,
-}
+// interface docListItemState {
+//   id: string
+//   title: string
+//   lastVisited: string
+//   role: string,
+// }
 
 const MainPage = () => {
-  const [docList, setDocList] = useState<docListItemState[]>([]);
+  // const [docList, setDocList] = useState<docListItemState[]>([]);
 
-  useEffect(() => {
-    const fetchDocList = async () => {
-      // TODO : request docList from API server
-      return new Promise((resolve) => {
-        resolve(setDocList(() => [{id:'1234',
-          title: '알고리즘 스터디 기록',
-          lastVisited: '2022-11-10',
-          role: 'onwer',  
-        }, {id:'5678',
-          title: 'Untitled',
-          lastVisited: '2022-11-19',
-          role: 'edit'}]));
-      });
-    };
-    fetchDocList();
-  }, []);
+  // useEffect(() => {
+  //   const fetchDocList = async () => {
+  //     // TODO : request docList from API server
+  //     return new Promise((resolve) => {
+  //       resolve(setDocList(() => [{id:'1234',
+  //         title: '알고리즘 스터디 기록',
+  //         lastVisited: '2022-11-10',
+  //         role: 'onwer',  
+  //       }, {id:'5678',
+  //         title: 'Untitled',
+  //         lastVisited: '2022-11-19',
+  //         role: 'edit'}]));
+  //     });
+  //   };
+  //   fetchDocList();
+  // }, []);
 
   return (
     <div>
-      {docList.length ? docList.map(doc => {
+      {/* {docList.length ? docList.map(doc => {
         return <DocListItem key={doc.id} id={doc.id} title={doc.title} lastVisited={doc.lastVisited} role={doc.role} />;
-      }) : <div>...loading</div>}
+      }) : <div>...loading</div>} */}
+      <DocList />
     </div>
   );
 };
