437a57323f077eecf41b6ccebd90b504c89a934c
iyu88 <iyu88@naver.com>
2022-12-01 01:29:23 +0900

    feat: #41 SVG 아이콘을 위한 컴포넌트 생성
    props 로 SVG 아이콘과 스타일 정보를 입력 받는 button 컴포넌트를 구현했습니다.

---
+++ frontend/src/components/iconButton/IconButton.tsx
@@ -0,0 +1,41 @@
+import React, { ReactNode } from 'react';
+import styled from 'styled-components';
+
+interface IconButtonProps {
+  children: ReactNode,
+  fill: string,
+  width?: string,
+  height?: string,
+  hover?: string,
+  active?: string,
+}
+
+const IconButton = (props: IconButtonProps) => {
+  const {children, ...iconButtonStyles} = props;
+
+  return (
+    <IconButtonWrapper {...iconButtonStyles}>
+      {children}
+    </IconButtonWrapper>
+  );
+};
+
+const IconButtonWrapper = styled.button<IconButtonProps>`
+  svg {
+    width: ${(props) => (props.width || 1) + 'rem'};
+    height: ${(props) => (props.height || 1) + 'rem'};
+    fill: ${(props) => props.fill};
+  }
+  &:hover {
+    svg {
+      fill: ${(props) => props.hover || 'black'};
+    }
+  }
+  &.active {
+    svg {
+      fill: ${(props) => props.active || 'black'};
+    }
+  }
+`; 
+
+export { IconButton };
