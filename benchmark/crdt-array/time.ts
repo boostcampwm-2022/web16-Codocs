5fc0307a6c1ebbd48dfc838baeb9fb26aff3357b
iyu88 <iyu88@naver.com>
2022-12-03 16:09:01 +0900

    test: #53 localInsert Worst 케이스 테스트
    localInsert 가 최악의 성능을 낼 수 있는 경우를 테스트합니다.
    글자와 글자 사이에 새로운 글자가 입력될 때의 실행 시간 성능을 측정합니다.
    
    Co-authored-by: komplamoose2 <iamjewel4445@gmail.com>

--- benchmark/crdt-array/time.ts
+++ benchmark/crdt-array/time.ts
@@ -1,5 +1,9 @@
 import { CRDT } from "./crdt/crdt";
 
+interface Template {
+  [key: string] : string
+}
+
 const calcTimeDiff = (testFunc : any, funcParams: any[], time: number, isStable : boolean) : string => {
   const startTime = performance.now();
   for (let i = 0; i < time ; i++) {
@@ -13,33 +17,67 @@ const calcTimeDiff = (testFunc : any, funcParams: any[], time: number, isStable 
   return `${endTime - startTime}ms`;
 }
 
-const localInsertTestSequence = () => {
-  const template = {
-    op_10 : "",
-    op_100 : "",
-    op_1000 : "",
-    op_10000 : "",
-    op_100000 : "",
+const localInsertBestCase = () => {
+  const template : Template = {
+    '10' : "",
+    '100' : "",
+    '1000' : "",
+    '10000' : "",
   };
 
-  let crdt = new CRDT();
-  template.op_10 = calcTimeDiff(crdt.localInsert.bind(crdt), ["a"], 10, true);
-
-  crdt = new CRDT();
-  template.op_100 = calcTimeDiff(crdt.localInsert.bind(crdt), ["a"], 100, true);
-
-  crdt = new CRDT();
-  template.op_1000 = calcTimeDiff(crdt.localInsert.bind(crdt), ["a"], 1000, true);
-
-  crdt = new CRDT();
-  template.op_10000 = calcTimeDiff(crdt.localInsert.bind(crdt), ["a"], 10000, true);
-
-  crdt = new CRDT();
-  template.op_100000 = calcTimeDiff(crdt.localInsert.bind(crdt), ["a"], 100000, true);
+  let crdt;
+  let operationTime;
+  let caseLength = Object.keys(template).length;
+  
+  
+  for (let i = 1; i <= caseLength; i++) {
+    crdt = new CRDT();
+    operationTime = Math.pow(10, i);
+    template[operationTime.toString()] = calcTimeDiff(crdt.localInsert.bind(crdt), ["a"], operationTime, true);
+  }
   
   console.table(template);
 }
 
-localInsertTestSequence();
+const localInsertWorstCase = () => {
+  const template: Template = {
+    '10' : "",
+    '100' : "",
+    '1000' : "",
+    '10000' : "",
+  };
 
+  let crdt;
+  let operationTime;
+  let caseLength = Object.keys(template).length;
 
+  for (let i = 1; i <= caseLength; i++) {
+    crdt = new CRDT();
+    operationTime = Math.pow(10, i);
+    template[operationTime.toString()] = calcTimeDiff(crdt.localInsert.bind(crdt), [0, "a"], operationTime, false)
+  }
+  
+  console.table(template);
+}
+
+const localInsertTestCase  = {
+  "BEST": () => {
+    localInsertBestCase();
+  },
+  "WORST" : () => {
+    localInsertWorstCase();
+  },
+  "RANDOM" : () => {
+    // localInsertRandomCase();
+  }
+}
+
+console.log("**1. LocalInsert 벤치마크**");
+console.log("1) 모든 입력이 Best하게 들어갈 경우");
+localInsertTestCase["BEST"]();
+
+console.log("2) 모든 입력이 Worst하게 들어갈 경우");
+localInsertTestCase["WORST"]();
+
+// console.log("3) 모든 입력이 랜덤하게 들어갈 경우");
+// localInsertTestCase["RANDOM"]();
