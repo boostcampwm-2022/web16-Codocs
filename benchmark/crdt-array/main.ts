acc60521c7bf17df0cc2262e4fdee1319d5115e3
iyu88 <iyu88@naver.com>
2022-12-03 17:39:01 +0900

    test: #53 localDelete 벤치마크 테스트
    CRDT 에 문자가 입력되어 있는 상태에서 localDelete 메서드의 실행 시간 성능을 테스트합니다.
    
    Co-authored-by: Boseok Kang <komplamoose@users.noreply.github.com>
    Co-authored-by: Kiseok <rltjr1092@gmail.com>

--- benchmark/crdt-array/main.ts
+++ benchmark/crdt-array/main.ts
@@ -1,5 +1,6 @@
 import { localInsertBenchmark } from "./localInsert";
 import { localInsertRangeBenchmark } from "./localInsertRange";
+import { localDeleteBenchmark } from './localDeleteTest';
 
 console.log("**1. LocalInsert 벤치마크**");
 console.log("1-1) 모든 입력이 Best하게 들어갈 경우");
@@ -21,3 +22,8 @@ localInsertRangeBenchmark("WORST");
 console.log("2-3) 모든 입력(3개를 한번에 입력)이 랜덤하게 들어갈 경우");
 localInsertRangeBenchmark("RANDOM");
 
+console.log("**3. LocalDelete 벤치마크**");
+console.log("3-1) 하나씩 삭제할 경우");
+console.log("* 여러 개 삭제하는 경우는 하나씩 삭제하는 경우와 동작이 같아 테스트하지 않는다.");
+console.log("* index는 존재하는 문자의 갯수와도 같음");
+localDeleteBenchmark("DELETE");
