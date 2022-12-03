67d06011dc2e174caee31a7132f4626fa0f4dcde
iyu88 <iyu88@naver.com>
2022-12-03 17:10:37 +0900

    test: #53 localInsertRange 벤치마크 테스트
    CRDT 의 localInsertRange 메서드의 실행 시간 성능을 벤치마크 테스트합니다.
    
    Co-authored-by: kiseoky <rltjr1092@gmail.com>
    Co-authored-by: komplamoose2 <iamjewel4445@gmail.com>

--- benchmark/crdt-array/main.ts
+++ benchmark/crdt-array/main.ts
@@ -1,11 +1,23 @@
 import { localInsertBenchmark } from "./localInsert";
+import { localInsertRangeBenchmark } from "./localInsertRange";
 
 console.log("**1. LocalInsert 벤치마크**");
-console.log("1) 모든 입력이 Best하게 들어갈 경우");
+console.log("1-1) 모든 입력이 Best하게 들어갈 경우");
 localInsertBenchmark("BEST");
 
-console.log("2) 모든 입력이 Worst하게 들어갈 경우");
+console.log("1-2) 모든 입력이 Worst하게 들어갈 경우");
 localInsertBenchmark("WORST");
 
-console.log("3) 모든 입력이 랜덤하게 들어갈 경우");
+console.log("1-3) 모든 입력이 랜덤하게 들어갈 경우");
 localInsertBenchmark("RANDOM");
+
+console.log("**2. LocalInsertRange 벤치마크**");
+console.log("2-1) 모든 입력(3개를 한번에 입력)이 Best하게 들어갈 경우");
+localInsertRangeBenchmark("BEST");
+
+console.log("2-2) 모든 입력(3개를 한번에 입력)이 Worst하게 들어갈 경우");
+localInsertRangeBenchmark("WORST");
+
+console.log("2-3) 모든 입력(3개를 한번에 입력)이 랜덤하게 들어갈 경우");
+localInsertRangeBenchmark("RANDOM");
+
