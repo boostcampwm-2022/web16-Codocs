25c0c230c203c48b39940b38cfed83d92081cf09
iyu88 <iyu88@naver.com>
2022-12-03 18:15:52 +0900

    test: #53 remoteInsert 벤치마크
    remoteInsert 메서드에 대해서 실행 시간 벤치마크 테스트 코드를 작성했습니다.
    
    Co-authored-by: kiseoky <rltjr1092@gmail.com>
    Co-authored-by: komplamoose2 <iamjewel4445@gmail.com>

--- benchmark/crdt-array/main.ts
+++ benchmark/crdt-array/main.ts
@@ -1,6 +1,7 @@
-import { localInsertBenchmark } from "./localInsert";
-import { localInsertRangeBenchmark } from "./localInsertRange";
+import { localInsertBenchmark } from "./localInsertTest";
+import { localInsertRangeBenchmark } from "./localInsertRangeTest";
 import { localDeleteBenchmark } from './localDeleteTest';
+import { remoteInsertBenchmark } from './remoteInsertTest';
 
 console.log("**1. LocalInsert 벤치마크**");
 console.log("1-1) 모든 입력이 Best하게 들어갈 경우");
@@ -27,3 +28,14 @@ console.log("3-1) 하나씩 삭제할 경우");
 console.log("* 여러 개 삭제하는 경우는 하나씩 삭제하는 경우와 동작이 같아 테스트하지 않는다.");
 console.log("* index는 존재하는 문자의 갯수와도 같음");
 localDeleteBenchmark("DELETE");
+
+console.log("**4. remoteInsert 벤치마크**");
+console.log("* 현재 양 쪽 모두 OperationCount만큼 인덱스가 존재하고 있음");
+console.log("4-1) 모든 remote 요청이 Best하게 들어갈 경우");
+remoteInsertBenchmark("BEST");
+
+console.log("4-2) 모든 remote 요청이 Worst하게 들어갈 경우");
+remoteInsertBenchmark("WORST");
+
+console.log("4-3) 모든 remote 요청이 랜덤하게 들어갈 경우");
+remoteInsertBenchmark("RANDOM");
