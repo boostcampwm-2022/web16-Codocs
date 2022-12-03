1ac73ee1efb35a8f3b9752e532161c730c0c57a5
iyu88 <iyu88@naver.com>
2022-12-03 14:56:53 +0900

    feat: #53 localInsert 벤치마크 성능 테스트
    localInsert 메서드를 테스트합니다.
    10, 100, 1000, 10000, 100000 개의 입력이 맨 뒤에 입력될 때의 시간을 측정합니다.
    
    Co-authored-by: komplamoose2 <iamjewel4445@gmail.com>

--- benchmark/crdt-array/time.ts
+++ benchmark/crdt-array/time.ts
@@ -1,12 +1,57 @@
-import {crdt} from "./crdt/crdt";
+import { CRDT } from "./crdt/crdt";
 
-const operation10 = () => {
-  console.log("Case 1 : LocalInsert 10 times");
-  console.time("Case 1 result : ");
-  for (let i=0; i<10; i++) {
-    crdt.localInsert(i, `${i}`);
+const localInsertTest = (times: number) => {
+  const template = {
+    op_10 : "",
+    op_100 : "",
+    op_1000 : "",
+    op_10000 : "",
+    op_100000 : "",
+  };
+  
+  console.log("*** LocalInsert() Test ***");
+  console.log("*** 1. 모든 입력이 Best하게 들어갈 경우 ***");
+  let crdt = new CRDT();
+  let startTime = performance.now()
+  for (let i = 0; i < times; i++) {
+    crdt.localInsert(i, `a`);
   }
-  console.timeEnd("Case 1 result : ");
+  let endTime = performance.now()
+  template.op_10 = `${endTime - startTime}ms`; 
+
+  crdt = new CRDT();
+  startTime = performance.now()
+  for (let i = 0; i < times*10; i++) {
+    crdt.localInsert(i, `a`);
+  }
+  endTime = performance.now()
+  template.op_100 = `${endTime - startTime}ms`; 
+
+  crdt = new CRDT();
+  startTime = performance.now()
+  for (let i = 0; i < times*100; i++) {
+    crdt.localInsert(i, `a`);
+  }
+  endTime = performance.now()
+  template.op_1000 = `${endTime - startTime}ms`; 
+
+  crdt = new CRDT();
+  startTime = performance.now()
+  for (let i = 0; i < times*1000; i++) {
+    crdt.localInsert(i, `a`);
+  }
+  endTime = performance.now()
+  template.op_10000 = `${endTime - startTime}ms`;
+
+  crdt = new CRDT();
+  startTime = performance.now()
+  for (let i = 0; i < times*10000; i++) {
+    crdt.localInsert(i, `a`);
+  }
+  endTime = performance.now()
+  template.op_100000 = `${endTime - startTime}ms`; 
+  
+  console.table(template);
 }
 
-operation10();
+localInsertTest(10);
