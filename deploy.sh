7811fa5c2351d54cdaee6885bc811345d8855331
kiseoky <rltjr1092@gmail.com>
2022-12-04 16:56:21 +0900

    nginx, 무중단 배포 설정 파일 업로드

---
+++ deploy.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+ 
+# Blue 를 기준으로 현재 떠있는 컨테이너를 체크한다.
+EXIST_BLUE=$(docker-compose -p api-blue -f docker-compose.blue.yaml ps | grep Up)
+ 
+# 컨테이너 스위칭
+if [ -z "$EXIST_BLUE" ]; then
+    echo "blue up"
+    docker-compose -p api-blue -f docker-compose.blue.yaml up -d
+    BEFORE_COMPOSE_COLOR="green"
+    AFTER_COMPOSE_COLOR="blue"
+else
+    echo "green up"
+    docker-compose -p api-green -f docker-compose.green.yaml up -d
+    BEFORE_COMPOSE_COLOR="blue"
+    AFTER_COMPOSE_COLOR="green"
+fi
+ 
+sleep 10
+ 
+# 새로운 컨테이너가 제대로 떴는지 확인
+EXIST_AFTER=$(docker-compose -p api-${AFTER_COMPOSE_COLOR} -f docker-compose.${AFTER_COMPOSE_COLOR}.yaml ps | grep Up)
+if [ -n "$EXIST_AFTER" ]; then
+    # nginx.config를 컨테이너에 맞게 변경해주고 reload 한다
+    sudo cp ./conf.d/nginx.${AFTER_COMPOSE_COLOR}.conf /etc/nginx/conf.d/default.conf
+    sudo systemctl restart nginx
+    # docker-compose -f docker-compose.nginx.yaml restart
+    # 이전 컨테이너 종료
+    docker-compose -p api-${BEFORE_COMPOSE_COLOR} -f docker-compose.${BEFORE_COMPOSE_COLOR}.yaml down
+    echo "$BEFORE_COMPOSE_COLOR down"
+fi
