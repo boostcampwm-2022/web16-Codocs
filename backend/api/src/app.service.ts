167c4140c7387e299d58d34564c0314104dd0d6f
Kiseok <rltjr1092@gmail.com>
2022-12-04 17:05:06 +0900

    무중단 배포 테스트!

--- backend/api/src/app.service.ts
+++ backend/api/src/app.service.ts
@@ -3,6 +3,6 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHello(): string {
-    return 'Hello World! blue';
+    return 'Hello World! green!';
   }
 }
