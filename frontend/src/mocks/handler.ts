ee5a8e5933630f6663033f3fd25538d1d4f2b65b
iyu88 <iyu88@naver.com>
2022-12-07 03:13:41 +0900

    feat: #40 userProfile generator 함수 정의

--- frontend/src/mocks/handler.ts
+++ frontend/src/mocks/handler.ts
@@ -1,21 +1,25 @@
 import { rest } from 'msw';
 import createDocumentList from './dummy/docList';
+import createUserProfile from './dummy/userProfile';
 
 interface generateEnumObj {
-  [key : string] : (counts: number) => DocListItem[];
+  [key : string] : (counts: number) => DocListItem[] | UserProfile[];
 }
 
 const generateFunc: generateEnumObj = {
   'docList' : createDocumentList,
+  'userProfile' : createUserProfile,
 };
 
 const generateDummy = (target: string, counts: number) => {
   return generateFunc[target](counts);
 };
 
-
 export const handler = [
   rest.get('/document/main', (req, res, ctx) => {
     return res(ctx.status(200), ctx.json(generateDummy('docList', 30)));
   }),
+  rest.get('/user/profile', (req, res, ctx) => {
+    return res(ctx.status(200), ctx.json(generateDummy('userProfile', 1)[0]));
+  })
 ];
