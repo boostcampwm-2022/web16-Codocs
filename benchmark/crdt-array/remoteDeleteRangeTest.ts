e214ab53f8ce522225c71c8c75f072ffcc8d7897
komplamoose2 <iamjewel4445@gmail.com>
2022-12-06 17:59:15 +0900

    feat: crdt-array main.ts 테스트케이스 수정
    Best, Worst 방식이 아닌 삽입 위치에 따른 케이스로 수정

--- benchmark/crdt-array/remoteDeleteRangeTest.ts
+++ benchmark/crdt-array/remoteDeleteRangeTest.ts
@@ -1,52 +1,62 @@
 import { CRDT } from "./crdt/crdt";
 
 interface Template {
-  [key: string] : string
+  [key: string]: string;
 }
 
 interface TestType {
-  [key: string] : Function
+  [key: string]: Function;
 }
 
 const testType: TestType = {
-  "DELETE" : (index: number, testFunc : any, funcParams: any[]) => {
+  DELETE: (index: number, testFunc: any, funcParams: any[]) => {
     testFunc(funcParams);
   },
-}
+};
 
-const calcTimeDiff = (testFunc : any, funcParams: any, time: number, type : string) : string => {
+const calcTimeDiff = (
+  testFunc: any,
+  funcParams: any,
+  time: number,
+  type: string
+): string => {
   const startTime = performance.now();
   testType[type](0, testFunc, funcParams);
   const endTime = performance.now();
   return `${endTime - startTime}ms`;
-}
+};
 
 const remoteDeleteRangeBenchmark = (type: string) => {
-  const template : Template = {
-    '10' : "",
-    '100' : "",
-    '1000' : "",
-    '10000' : "",
+  const template: Template = {
+    "10": "",
+    "100": "",
+    "1000": "",
+    //'10000' : "", 실행 불가, 30분 이상 대기
   };
 
   let crdt;
   let operationTime;
   let caseLength = Object.keys(template).length;
-  
+
   for (let i = 1; i <= caseLength; i++) {
     crdt = new CRDT();
     const crdtOthers = new CRDT();
     operationTime = Math.pow(10, i);
-    for(let j = 0; j < operationTime; j++) {
-        crdt.localInsert(0, 'a');
-        crdtOthers.localInsert(0, 'a');
+    for (let j = 0; j < operationTime; j++) {
+      crdt.localInsert(0, "a");
+      crdtOthers.localInsert(0, "a");
     }
     let operationHistory = crdtOthers.localDelete(0, operationTime);
-    
-    template[operationTime.toString()] = calcTimeDiff(crdt.remoteDeleteRange.bind(crdt), operationHistory, operationTime, type);
+
+    template[operationTime.toString()] = calcTimeDiff(
+      crdt.remoteDeleteRange.bind(crdt),
+      operationHistory,
+      operationTime,
+      type
+    );
   }
-  
+
   console.table(template);
-}
+};
 
 export { remoteDeleteRangeBenchmark };
--- benchmark/crdt-array/remoteDeleteTest.ts
+++ benchmark/crdt-array/remoteDeleteTest.ts
@@ -1,51 +1,60 @@
 import { CRDT } from "./crdt/crdt";
 
 interface Template {
-  [key: string] : string
+  [key: string]: string;
 }
 
 interface TestType {
-  [key: string] : Function
+  [key: string]: Function;
 }
 
 const testType: TestType = {
-  "DELETE" : (index: number, testFunc : any, funcParams: any[]) => {
+  DELETE: (index: number, testFunc: any, funcParams: any[]) => {
     testFunc(funcParams);
   },
-}
+};
 
-const calcTimeDiff = (testFunc : any, funcParams: any, time: number, type : string) : string => {
+const calcTimeDiff = (
+  testFunc: any,
+  funcParams: any,
+  time: number,
+  type: string
+): string => {
   const startTime = performance.now();
-  for (let i = 0; i < time ; i++) {
+  for (let i = 0; i < time; i++) {
     testType[type](i, testFunc, funcParams[i]);
   }
   const endTime = performance.now();
   return `${endTime - startTime}ms`;
-}
+};
 
 const remoteDeleteBenchmark = (type: string) => {
-  const template : Template = {
-    '10' : "",
-    '100' : "",
-    '1000' : "",
-    '10000' : "",
+  const template: Template = {
+    "10": "",
+    "100": "",
+    "1000": "",
   };
 
   let crdt;
   let operationTime;
   let caseLength = Object.keys(template).length;
-  
+
   for (let i = 1; i <= caseLength; i++) {
     crdt = new CRDT();
     operationTime = Math.pow(10, i);
-    let operationHistory = []
-    for(let j = 0; j < operationTime; j++) {
-        operationHistory.push(crdt.localInsert(0, 'a'));
+    let operationHistory = [];
+    for (let j = 0; j < operationTime; j++) {
+      operationHistory.push(crdt.localInsert(0, "a"));
     }
-    template[operationTime.toString()] = calcTimeDiff(crdt.remoteDelete.bind(crdt), operationHistory, operationTime, type);
+    template[operationTime.toString()] = calcTimeDiff(
+      crdt.remoteDelete.bind(crdt),
+      operationHistory,
+      operationTime,
+      type
+    );
   }
-  
+
   console.table(template);
-}
+};
 
 export { remoteDeleteBenchmark };
