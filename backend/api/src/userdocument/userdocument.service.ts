1630904276920accb98d8e9ccb74822f07640eeb
k-seok <rltjr1092@gmail.com>
2022-12-08 00:53:13 +0900

    chore: #79 console log 제거

--- backend/api/src/userdocument/userdocument.service.ts
+++ backend/api/src/userdocument/userdocument.service.ts
@@ -35,9 +35,8 @@ export class UserDocumentService {
       where: { user: { email } },
       order: { lastVisited: 'DESC' }
     });
-    const ret = userDocuments.map((userDocument) => new UserDocumentResponseDTO(userDocument));
-    console.log(ret);
-    return ret;
+
+    return userDocuments.map((userDocument) => new UserDocumentResponseDTO(userDocument));
   }
 
   findOne(id: string) {
