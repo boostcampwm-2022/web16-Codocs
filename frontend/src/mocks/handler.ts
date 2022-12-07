e1aaf6d6ec0f4a6e9407838d996c902f36496301
iyu88 <iyu88@naver.com>
2022-12-07 15:42:11 +0900

    feat: 새로운 문서 id 생성 API Mockup
    새로운 문서 생성 버튼을 눌렀을 때 서버에서 uuid 를 생성하는 API 를 목업합니다.

--- frontend/src/mocks/handler.ts
+++ frontend/src/mocks/handler.ts
@@ -1,17 +1,19 @@
 import { rest } from 'msw';
 import createDocumentList from './dummy/docList';
+import createDocumentId from './dummy/document';
 import createUserProfile from './dummy/userProfile';
 
 interface generateEnumObj {
-  [key : string] : (counts: number) => DocListItem[] | UserProfile[];
+  [key : string] : (counts: number) => DocListItem[] | UserProfile[] | string;
 }
 
 const generateFunc: generateEnumObj = {
   'docList' : createDocumentList,
+  'document': createDocumentId, 
   'userProfile' : createUserProfile,
 };
 
-const generateDummy = (target: string, counts: number) => {
+const generateDummy = (target: string, counts = 1) => {
   return generateFunc[target](counts);
 };
 
@@ -19,7 +21,10 @@ export const handler = [
   rest.get('/document/main', (req, res, ctx) => {
     return res(ctx.status(200), ctx.json(generateDummy('docList', 30)));
   }),
+  rest.get('/document/new', (req, res, ctx) => {
+    return res(ctx.status(200), ctx.json(generateDummy('document')));
+  }),
   rest.get('/user/profile', (req, res, ctx) => {
-    return res(ctx.status(200), ctx.json(generateDummy('userProfile', 1)[0]));
-  })
+    return res(ctx.status(200), ctx.json(generateDummy('userProfile')[0]));
+  }),
 ];
