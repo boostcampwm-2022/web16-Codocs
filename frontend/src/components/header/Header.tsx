baec80961763659bd187c22848c4255bb1d7259d
iyu88 <iyu88@naver.com>
2022-12-07 22:34:49 +0900

    feat: #64 Header 중앙에 사용자 위치 표시
    URL 에 대응하는 페이지 이름을 Header 에 표시하기 위해서 useLocation 을 사용했습니다.
    객체의 key 값을 도메인 주소로 하여 대응되는 value 를 pageTitle 로 사용합니다.

--- frontend/src/components/header/Header.tsx
+++ frontend/src/components/header/Header.tsx
@@ -1,9 +1,28 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import styled from 'styled-components';
+import { useLocation } from 'react-router-dom';
 import { SiteLogo } from '../siteLogo';
 import { UserProfile } from '../userProfile';
 
+interface pageTitles {
+  [key: string] : string, 
+}
+
 const Header = () => {
+  const [currentPath, setCurrentPath] = useState('');
+  const location = useLocation();
+
+  const pageTitleByPath: pageTitles = {
+    'main': '최근 문서 목록',
+    'private' : '내 문서함',
+    'shared' : '공유 문서함',
+  };
+
+  useEffect(() => {
+    const lastPath = location.pathname.split('/').slice(-1);
+    setCurrentPath(pageTitleByPath[lastPath[0]]);
+  }, [location]);
+  
   const profileProps = {
     profileImgURL: 'https://picsum.photos/200',
     userName: 'iyu88'
@@ -12,7 +31,7 @@ const Header = () => {
   return (
     <HeaderWrapper>
       <SiteLogo />
-      <PageTitle>최근 문서 목록</PageTitle>
+      <PageTitle>{currentPath}</PageTitle>
       <UserProfile {...profileProps}/>
     </HeaderWrapper>
   );
