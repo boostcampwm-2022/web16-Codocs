5604bf2a50a4a1bb8a2d3c9b2656986ff981083f
iyu88 <iyu88@naver.com>
2022-12-07 19:22:11 +0900

    feat: #22 새로운 문서를 생성하는 버튼 구현
    새로운 문서를 생성하는 NewDocBtn 의 스타일을 구현했습니다.

--- frontend/src/pages/MainPage.tsx
+++ frontend/src/pages/MainPage.tsx
@@ -3,14 +3,23 @@ import styled from 'styled-components';
 import { Header } from '../components/header';
 import { SideBar } from '../components/sideBar';
 import { DocListContainer } from '../components/docListContainer';
+import { IconButton } from '../components/iconButton';
+import { ReactComponent as PencilIcon } from '../../src/assets/pencil.svg';
 
 const MainPage = () => {
+
   return (
     <>
       <Header />
       <Container>
         <SideBar />
         <ContentWrapper>
+          <NewDocBtn>
+            <IconButton fill={'#FFFFFF'} width={'1.5'} height={'1.5'} >
+              <PencilIcon />
+            </IconButton>
+            <BtnText>새로운 문서 작성</BtnText>
+          </NewDocBtn>
           <ContentHeaderGroup>
             <Title>최근 문서 목록</Title>
             <div>드롭다운</div>
@@ -29,7 +38,7 @@ const Container = styled.main`
 
 const ContentWrapper = styled.section`
   flex: 1;
-  margin: 2rem 3.5rem;
+  margin: 3rem 3.5rem;
   overflow-y: scroll;
 `;
 
@@ -43,6 +52,33 @@ const ContentHeaderGroup = styled.div`
 const Title = styled.h1`
   font-weight: 800;
   font-size: 2rem;
+`;
+
+const NewDocBtn = styled.button`
+  display: flex;
+  align-items: center;
+  border-radius: 10px;
+  border: 1px solid #3A7DFF;
+  background-color: #3A7DFF;
+  padding: 1.25rem 2rem 1.25rem 1.5rem;
+  margin-bottom: 2rem;
+  &:hover {
+    span {
+      color: #3A7DFF;
+    }
+    svg {
+      fill: #3A7DFF;
+    }
+    background-color: #FFFFFF;
+  }
+`;
+  
+const BtnText = styled.span`
+  font-weight: 500;
+  font-size: 20px;
+  color: #FFFFFF;
+  text-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25);
+  margin-left: 0.5rem;
 `;
 
 export default MainPage;
