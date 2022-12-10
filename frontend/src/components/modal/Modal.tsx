0ec85f360241fda90b2e3ed77108d6433e236a89
iyu88 <iyu88@naver.com>
2022-12-10 16:28:47 +0900

    feat: #86 클릭 이벤트 제한
    useRef 를 사용하여 Dimmed 를 저장하고 우선적으로 Dimmed 영역을 눌렀을 때에 Modal 이 사라지도록 했습니다.

--- frontend/src/components/modal/Modal.tsx
+++ frontend/src/components/modal/Modal.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useRef } from 'react';
 import styled from 'styled-components';
 import { useRecoilState } from 'recoil';
 import { modalState } from '../../atoms/modalAtom';
@@ -13,54 +13,56 @@ interface AnswerBtnProps {
 
 const Modal = () => {
   const [isVisible, setIsVisible] = useRecoilState(modalState);
+  const dimmedRef = useRef<HTMLDivElement | null>(null);
 
   const handleCloseModal = (e: React.MouseEvent<HTMLDivElement | HTMLButtonElement>) => {
     e.preventDefault();
-    e.stopPropagation();
-    setIsVisible(false);
-    console.log(isVisible);
+    if (e.target === dimmedRef.current) {
+      setIsVisible(false);
+      console.log(isVisible);
+    }
   };
 
   return (
-      <Dimmed isVisible={isVisible} onClick={handleCloseModal}>
+      <Dimmed ref={dimmedRef} isVisible={isVisible} onClick={handleCloseModal}>
         <ModalContent>
-        <Question>
-          <QuestionTitle>
-          정말로 삭제하시겠습니까?
-          </QuestionTitle>
-          <QuestionDescribtion>
-            한 번 삭제한 문서는 복구할 수 없습니다.
-            그래도 진행하시겠습니까?
-          </QuestionDescribtion>
-        </Question>
-        <Answers>
-          <AnswerBtn backgroundColor={'#A5A5A5'}>취소</AnswerBtn>
-          <AnswerBtn backgroundColor={'#FF5757'}>확인</AnswerBtn>
-        </Answers>
+          <Question>
+            <QuestionTitle>
+            정말로 삭제하시겠습니까?
+            </QuestionTitle>
+            <QuestionDescribtion>
+              한 번 삭제한 문서는 복구할 수 없습니다.
+              그래도 진행하시겠습니까?
+            </QuestionDescribtion>
+          </Question>
+          <Answers>
+            <AnswerBtn backgroundColor={'#A5A5A5'}>취소</AnswerBtn>
+            <AnswerBtn backgroundColor={'#FF5757'}>확인</AnswerBtn>
+          </Answers>
         </ModalContent>
       </Dimmed>
   );
 };
 
 const Dimmed = styled('div')<DimmedProps>`
-  display: flex;
-  justify-content: center;
-  align-items: center;
   position: fixed;
   width: 100%;
   height: 100%;
   top: 0;
   left: 0;
-  background-color: rgba(30,30,30,0.9);
-  cursor: pointer;
+  background-color: rgba(30, 30, 30, 0.9);
+  z-index: 1000;
   visibility: ${(props) => props.isVisible ? 'visible': 'hidden'};
 `;
 
 const ModalContent = styled.div`
-  min-width: 550px;
+  width: 550px;
+  min-height: 450px;
   border-radius: 10px;
   background-color: #DFDFDF;
   padding: 6.5rem 2rem;
+  margin: 0 auto;
+  margin-top: 4rem;
 `;
 
 const Question = styled.div`
@@ -79,6 +81,7 @@ const QuestionTitle = styled.h2`
 const QuestionDescribtion = styled.p`
   max-width: 330px;
   word-break: keep-all;
+  margin-top: 0;
 `;
 
 const Answers = styled.div`
