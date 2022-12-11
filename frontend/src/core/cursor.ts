96bac0c02d7384cb3f6434f2e6ba4e8176bcd783
komplamoose2 <iamjewel4445@gmail.com>
2022-12-12 02:38:23 +0900

    feat: #58 Cursor Hover시 커서 이름 표시

--- frontend/src/core/cursor.ts
+++ frontend/src/core/cursor.ts
@@ -7,23 +7,49 @@ class Cursor {
 
   name: string;
 
+  height: number;
+
   constructor(color: string, name: string) {
     this.color = color;
     this.name = name;
+    this.height = 0;
   }
 
   updateCursor(editor: CodeMirror.Editor, cursorPosition: CodeMirror.Position) {
     this.removeCursor();
     const cursorCoords = editor.cursorCoords(cursorPosition);
     const cursorHolder = document.createElement('span');
-    const height = cursorCoords.bottom - cursorCoords.top;
+    this.height = cursorCoords.bottom - cursorCoords.top;
 
+    cursorHolder.classList.add('remote-cursor');
     cursorHolder.style.borderLeftColor = this.color;
-    cursorHolder.style.height = `${height}px`;
+    cursorHolder.style.height = `${this.height}px`;
+
+    this.showCursorName(cursorHolder);
 
     this.marker = editor.setBookmark(cursorPosition, {
       widget: cursorHolder,
       insertLeft: true
+    });
+  }
+
+  private showCursorName(cursorHolder: HTMLSpanElement) {
+    const nameHolder = document.createElement('span');
+    nameHolder.classList.add('remote-cursor-name');
+
+    cursorHolder.addEventListener('mouseenter', () => {
+      nameHolder.textContent = this.name;
+      nameHolder.style.top = `-${this.height}px`;
+      nameHolder.style.backgroundColor = this.color;
+
+      nameHolder.classList.remove('hide');
+      cursorHolder.appendChild(nameHolder);
+    });
+
+    cursorHolder.addEventListener('mouseleave', () => {
+      nameHolder.classList.add('hide');
+
+      nameHolder.parentNode?.removeChild(nameHolder);
     });
   }
 
