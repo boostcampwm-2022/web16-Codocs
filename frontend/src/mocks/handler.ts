d11047fce7059bf968a657912680ac30168a8715
iyu88 <iyu88@naver.com>
2022-12-14 13:42:03 +0900

    test: Mock API 주석
    실제 DB 주소와 싱크하기 전 Mock API 를 주석 처리합니다.

--- frontend/src/mocks/handler.ts
+++ frontend/src/mocks/handler.ts
@@ -22,29 +22,28 @@ const dummyDocList = generateDummy('docList', 30) as DocListItem[];
 
 export const handler = [
   rest.get('/user-document/recent', (req, res, ctx) => {
-    const recentDocList = dummyDocList.filter(doc => doc.isDeleted === false);
-    return res(ctx.status(200), ctx.json(recentDocList));
+    return res(ctx.status(200), ctx.json(dummyDocList));
   }),
-  rest.get('/user-document/shared', (req, res, ctx) => {
-    // TODO: 공유된 문서만 반환하도록 수정 
-    const sharedDocList = dummyDocList.filter(doc => doc.shared === true && doc.isDeleted === false);
-    return res(ctx.status(200), ctx.json(sharedDocList));
-  }),
-  rest.get('/bookmark', (req, res, ctx) => {
-    // TODO: 북마크된 문서만 반환하도록 수정
-    const bookmarkDocList = dummyDocList.filter(doc => doc.bookmark === true && doc.isDeleted === false); 
-    return res(ctx.status(200), ctx.json(bookmarkDocList));
-  }),
-  rest.post('/bookmark/:id', (req, res, ctx) => {
-    dummyDocList.some(doc => {
-      if (doc.id === req.params.id) {
-        doc.bookmark = true;
-        return true;
-      }
-      return false;
-    });
-    return res(ctx.status(200));
-  }),
+  // rest.get('/user-document/shared', (req, res, ctx) => {
+  //   // TODO: 공유된 문서만 반환하도록 수정 
+  //   const sharedDocList = dummyDocList.filter(doc => doc.shared === true && doc.isDeleted === false);
+  //   return res(ctx.status(200), ctx.json(sharedDocList));
+  // }),
+  // rest.get('/bookmark', (req, res, ctx) => {
+  //   // TODO: 북마크된 문서만 반환하도록 수정
+  //   const bookmarkDocList = dummyDocList.filter(doc => doc.bookmark === true && doc.isDeleted === false); 
+  //   return res(ctx.status(200), ctx.json(bookmarkDocList));
+  // }),
+  // rest.post('/bookmark/:id', (req, res, ctx) => {
+  //   dummyDocList.some(doc => {
+  //     if (doc.id === req.params.id) {
+  //       doc.bookmark = true;
+  //       return true;
+  //     }
+  //     return false;
+  //   });
+  //   return res(ctx.status(200));
+  // }),
   rest.get('/document/:id', (req, res, ctx) => {
     const [ targetDocument ] = dummyDocList.filter(doc => doc.id === req.params.id);
     return res(ctx.status(200), ctx.json(
@@ -52,16 +51,16 @@ export const handler = [
       content: charMap }
     ));
   }),
-  rest.delete('/document/:id', (req, res, ctx) => {
-    dummyDocList.some(doc => {
-      if (doc.id === req.params.id) {
-        doc.isDeleted = true;
-        return true;
-      }
-      return false;
-    });
-    return res(ctx.status(200));
-  }),
+  // rest.delete('/document/:id', (req, res, ctx) => {
+  //   dummyDocList.some(doc => {
+  //     if (doc.id === req.params.id) {
+  //       doc.isDeleted = true;
+  //       return true;
+  //     }
+  //     return false;
+  //   });
+  //   return res(ctx.status(200));
+  // }),
   rest.post('/document', (req, res, ctx) => {
     const newDocument = generateDummy('newDocument') as DocListItem;
     dummyDocList.push(newDocument);
