339c927d3f750fe6b88de67b761bdc442b14a1c5
k-seok <rltjr1092@gmail.com>
2022-12-15 03:43:46 +0900

    feat: #105 북마크 페이지 추가
    Co-authored-by: Hyunbin Lee <iyu88@users.noreply.github.com>
    Co-authored-by: Boseok Kang <komplamoose@users.noreply.github.com>

---
+++ frontend/src/pages/BookmarkPage.tsx
@@ -0,0 +1,43 @@
+import React, { useState, Suspense } from 'react';
+import styled from 'styled-components';
+import usePageName from '../hooks/usePageName';
+import { DocList } from '../components/docList';
+import { Spinner } from '../components/spinner';
+import { Dropdown } from '../components/dropdown';
+
+const BookmarkPage = () => {
+  const { pageName } = usePageName();
+  const [selectedOption, setSelectedOption] = useState<string>('lastVisited');
+
+  return (
+    <ContentWrapper>
+      <ContentHeaderGroup>
+        <PageName>{pageName}</PageName>
+        <Dropdown selectedOption={selectedOption} selectedOptionSetter={setSelectedOption} />
+      </ContentHeaderGroup>
+      <Suspense fallback={<Spinner />}>
+        <DocList documentType={'bookmark'} sortOption={selectedOption} />
+      </Suspense>
+    </ContentWrapper>
+  );
+};
+
+const ContentWrapper = styled.section`
+  flex: 1;
+  margin: 3rem 3.5rem;
+  overflow-y: scroll;
+`;
+
+const ContentHeaderGroup = styled.div`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  margin-bottom: 2rem;
+`;
+
+const PageName = styled.h1`
+  font-weight: 800;
+  font-size: 2rem;
+`;
+
+export default BookmarkPage;
