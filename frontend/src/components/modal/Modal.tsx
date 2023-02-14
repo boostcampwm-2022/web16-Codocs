f03913148d2774a3f1c999b7d363bad67e2185b8
iyu88 <iyu88@naver.com>
2023-02-15 02:39:43 +0900

    refactor: Modal Portal 생성
    Modal 을 전역 상태로 관리하지 않기 위해서 React 의 Portal 기능을 이용합니다.
    Portal 을 설정하고 Modal 컴포넌트를 수정했습니다.
    Modal 의 상태와 내부 UI 를 외부에 위임합니다.

--- frontend/src/components/modal/Modal.tsx
+++ frontend/src/components/modal/Modal.tsx
@@ -1,40 +1,24 @@
-import React from 'react';
+import React, { ReactNode } from 'react';
 import styled from 'styled-components';
-import { ModalDoubleChecker } from '../modalDoubleChecker';
-import { useRecoilState } from 'recoil';
-import { modalState } from '../../atoms/modalAtom';
+import ModalPortal from './ModalPortal';
 
-interface DimmedProps {
-  modalType: string;
+interface ModalProps {
+  children: ReactNode,
+  setOnModal: (state: boolean) => void,
 }
 
-const Modal = () => {
-  const [modalData, setModalData] = useRecoilState(modalState);
-
-  const handleInitModal = (e: React.MouseEvent) => {
-    e.preventDefault();
-    setModalData({
-      type: 'INIT',
-      clickHandler: () => {
-        return Promise<void>;
-      }
-    });
-  };
+const Modal = ({children, setOnModal}: ModalProps) => {
 
   return (
-    <Dimmed modalType={modalData.type}>
-      {modalData.type !== 'INIT' && (
-        <ModalDoubleChecker
-          modalType={modalData.type}
-          modalCancelHandler={handleInitModal}
-          modalActionHandler={modalData.clickHandler}
-        />
-      )}
-    </Dimmed>
+    <ModalPortal>
+      <Dimmed onClick={() => setOnModal(false)}>
+          {children}
+      </Dimmed>
+    </ModalPortal>
   );
 };
 
-const Dimmed = styled('div')<DimmedProps>`
+const Dimmed = styled.div`
   position: fixed;
   width: 100%;
   height: 100%;
@@ -42,7 +26,6 @@ const Dimmed = styled('div')<DimmedProps>`
   left: 0;
   background-color: rgba(30, 30, 30, 0.9);
   z-index: 1000;
-  display: ${(props) => (props.modalType === 'INIT' ? 'none' : 'block')};
 `;
 
 export { Modal };
